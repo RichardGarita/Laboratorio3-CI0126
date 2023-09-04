309ce7fd7463b1dda70c084e97eb96256db4b73d
RichardGarita <richard.garita@ucr.ac.cr>
2023-09-04 13:48:04 -0600

    Creado song Controller

---
+++ Laboratorio3/Laboratorio3/Controllers/SongController.cs
@@ -0,0 +1,29 @@
+﻿using Laboratorio3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Laboratorio3.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+            var song = GetSong();
+            ViewBag.MainTitle = "My Favourite Song";
+            return View();
+        }
+
+        private SongModel GetSong()
+        {
+            var song = new SongModel();
+            song.Name = "Radioactive";
+            song.Genre = "Alternative Rock";
+            song.Author = "Imagine Dragons";
+            song.Album = "Night Visions (Deluxe)";
+            song.ReleasedDate = new DateTime(2014, 01, 27);
+
+            return song;
+        }
+    }
+}
